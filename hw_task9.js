e4c36c109782577511fb9517687e5594a35f3e82
Arkadiy-S <avs21039@gmail.com>
2017-05-16 18:00:01 +0300

    last and tested

--- hw_task9.js
+++ hw_task9.js
@@ -10,41 +10,73 @@
 	207
 
 */
-"use strict";
+//"use strict";
  
 function rpn(s) {
 
 let i;
 let arrS = s.split(" "); //массив для хранения исходной строки
 let stack = []; // стек для обработки
-let output; // результат
-let nTmp;
+let output = 0; // результат
+let op1 = 0; // переменная для 1-го операнда
+let op2 = 0; // переменная для 2-го операнда
+let nTmp = 0; // переменная для 2-го операнда
 
 
 
-arr.forEach(function(item, i, arrS) { // перебираем  символы исходной строки из массива arr
-  
-  // модуль анализа поступающих символов исходной строки из массива arr  
-  if ( typeof item == "number") stack.push(item)  //  если  "на вход" анализа  поступает  число - заносится в стек
+//arrS.forEach(function(item) { // перебираем  символы исходной строки из массива arr
+ for (i=0; i<arrS.length; i++) {
+  // модуль анализа поступающих символов исходной строки из массива arr 
+  let item = arrS[i]; 
+  if (item == "") {
+    output = "ERROR";
+    stack.push(output);
+    break;
+  }
+  if (!isNaN(item)) stack.push(+item)  //  если  "на вход"   поступает  число - заносится в стек
       else {
-        case item
-      
-    
+        switch (item) { //  если  "на вход"   поступает знак операции - производится вычисление
+          case "+":
+            //op1 = +stack.pop();
+            //op2 = +stack.pop();
+            //output = op1 + op2;
+            nTmp = stack.pop();
+            output = stack.pop() + nTmp;
+            stack.push(output);
+            //continue;
+            break;
+          case "-":
+            nTmp = stack.pop();
+            output = stack.pop() - nTmp;
+            stack.push(output);
+            //continue;
+            break;
+          case "*":
+          nTmp = stack.pop();
+            output = stack.pop() * nTmp;
+            stack.push(output);
+            //continue;
+            break;
+          case "/":
+            nTmp = stack.pop();
+            output = stack.pop() / nTmp;
+            stack.push(output);
+            //continue;
+            break;
+          default:
+            output = "ERROR";
+            stack.push(output);
+        }
       }
-  
 
+ 
+};
 
-
-  
-});
-
-
-if ( stack.length != 0 ) {output = false;} // последовательность символов закончилась , стек не опустел 
-// - неверная последовательность 
-
+output =  stack.pop();  
+if ( stack.length != 0 ) {output = "ERROR";} // последовательность символов закончилась , стек не опустел     
 return output; 
 }
 
 // Input: 234 345 456 + + 5 /
 let s = "234 345 456 + + 5 /";
-console.log(rpn(s));
+console.log("Output = " + rpn(s));
